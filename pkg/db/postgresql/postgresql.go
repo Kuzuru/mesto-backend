51dd288b470a40e039f3ebf3e76c897ff15047ee
Artem Kiselev <kuzuru.dev@gmail.com>
2022-08-13 11:02:48 +0500

    feat(pkg/db): Implemented Client for PostgreSQL

---
+++ pkg/db/postgresql/postgresql.go
@@ -0,0 +1,44 @@
+package postgresql
+
+import (
+	"context"
+	"fmt"
+	"github.com/jackc/pgx/v4/pgxpool"
+	"github.com/rs/zerolog/log"
+	"mesto/utils"
+	"os"
+	"time"
+
+	"github.com/jackc/pgconn"
+	"github.com/jackc/pgx/v4"
+)
+
+type Client interface {
+	Exec(ctx context.Context, sql string, arguments ...interface{}) (pgconn.CommandTag, error)
+	Query(ctx context.Context, sql string, args ...interface{}) (pgx.Rows, error)
+	QueryRow(ctx context.Context, sql string, args ...interface{}) pgx.Row
+	Begin(ctx context.Context) (pgx.Tx, error)
+}
+
+func NewClient(ctx context.Context, maxConnectionAttempts int) (pool *pgxpool.Pool, err error) {
+	DSN := fmt.Sprintf("postgresql://%s:%s@%s:%s/%s", os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_NAME"))
+
+	// Trying to connect to postgres
+	err = utils.DoWithTries(func() error {
+		ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+		defer cancel()
+
+		pool, err = pgxpool.Connect(ctx, DSN)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	}, maxConnectionAttempts, 5*time.Second)
+
+	if err != nil {
+		log.Fatal().Stack().Msg("Failed to connect to postgres")
+	}
+
+	return pool, nil
+}
