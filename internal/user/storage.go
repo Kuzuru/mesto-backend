3ae29e1f33e8f4a053987c43607a68960bac6c5c
Artem Kiselev <kuzuru.dev@gmail.com>
2022-08-13 13:30:11 +0500

    fix(user/storage): Updated FindOne signature

--- internal/user/storage.go
+++ internal/user/storage.go
@@ -7,7 +7,7 @@ import (
 type Storage interface {
 	Create(ctx context.Context, user *User) error
 	FindAll(ctx context.Context) (u []User, err error)
-	FindOne(ctx context.Context, AuthID string) (User, error)
+	FindOne(ctx context.Context, AuthID string) (*User, error)
 	UpdateProfile(ctx context.Context, user User) error
 	UpdateAvatar(ctx context.Context, user User) error
 	Delete(ctx context.Context, id string) error
